4db07b75d77ba60ae4c441216d5151a041c509ff
kotjuz <rkotarski10@gmail.com>
2024-12-10 16:39:46 +0100

    fixed the space bug

--- player.py
+++ player.py
@@ -20,13 +20,46 @@ class Player:
 
     def check_for_space(self, how_many, pick):
         ile = 0
-        for i in range(pick - 1, 100, 10):
+
+        for i in range(pick - 1 + 10, 100, 10):
             if self.board[i] != "S":
                 ile += 1
                 if ile >= how_many:
                     return True
             else:
                 break
+
+        for i in range(pick -1 - 10, -1, -10):
+            if self.board[i] != "S":
+                ile += 1
+                if ile >= how_many:
+                    return True
+            else:
+                break
+
+        ile = 0
+        tmp_pick = pick
+        last_digit = pick % 10
+        if last_digit == 0:
+            last_digit = 10
+        for i in range(last_digit, 10):
+            if self.board[tmp_pick - 1] != "S":
+                ile += 1
+                tmp_pick += 1
+                if ile >= how_many:
+                    return True
+            else:
+                break
+
+        for i in range(last_digit - 1, 0, -1):
+            if self.board[tmp_pick - 1] != "S":
+                ile += 1
+                tmp_pick -= 1
+                if ile >= how_many:
+                    return True
+            else:
+                break
+
         return False
 
     def set_board(self):
@@ -36,7 +69,9 @@ class Player:
                 print(f"Ustaw swój {size}-masztowiec!")
                 while len(picks) < size:
                     pick = int(input(f"Wybierz {len(picks) + 1} miejsce: "))
-                    if pick < 1 or pick > 100:
+                    if len(picks) < 1 and not self.check_for_space(size, pick):
+                        print("Nie możesz tutaj postawić statku, ponieważ nie zmieścisz go calego. Spróbuj ponownie.")
+                    elif pick < 1 or pick > 100:
                         print("Twój wybór musi być w przedziale 1-100. Spróbuj ponownie.")
                     elif self.board[pick - 1] == "S":
                         print("To pole jest już zajęte. Spróbuj ponownie.")
